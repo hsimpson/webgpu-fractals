5ef56eaeab08f78e552a567999b02de5ac8d4665
Daniel Toplak <homer.simpson123@gmx.de>
2025-04-08 17:50:26 +0200

    feat: update packages

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,22 +1,14 @@
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'node:path';
 import process from 'node:process';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 8082,
-    host: '0.0.0.0',
-  },
+  plugins: [react(), tailwindcss()],
   root: 'src',
   publicDir: '../public',
-  build: {
-    minify: true,
-    outDir: '../dist',
-  },
-  resolve: {
-    alias: { '/src': path.resolve(process.cwd(), 'src') },
-  },
+  build: { minify: true, outDir: '../dist' },
+  resolve: { alias: { '/src': path.resolve(process.cwd(), 'src') } },
   base: '',
 });
